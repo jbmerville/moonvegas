6e773cbf4132633a87d58a66909d47d129fe0da6
jbmerville <jb.merville@gmail.com>
2022-09-11 20:41:40 +0100

    feat: display ticket differently if it is owned by the current account

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -24,8 +24,8 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       />
       <div className='layout z-50 flex items-center justify-between '>
         <div className='mb:text flex w-full items-center justify-center pt-2 pb-6 text-center text-xs text-white/50'>
-          Moonbeam Raffle is not available in Excluded Jurisdictions. By accessing and using the
-          interface you agree with our Terms & Conditions.
+          MoonVegas is not available in Excluded Jurisdictions. By accessing and using the interface
+          you agree with our Terms & Conditions.
         </div>
       </div>
     </>
--- src/components/ticket/Ticket.tsx
+++ src/components/ticket/Ticket.tsx
@@ -1,3 +1,4 @@
+import { useEthers } from '@usedapp/core';
 import Image from 'next/image';
 import React, { useEffect, useState } from 'react';
 
@@ -12,6 +13,7 @@ interface TicketPropsType {
 const Ticket = (props: TicketPropsType) => {
   const [isOwner, setIsOwner] = useState(false);
   const [isHover, setIsHover] = useState(false);
+  const { account } = useEthers();
 
   useEffect(() => {
     setIsOwner(props.ticket.owner !== undefined);
@@ -21,7 +23,33 @@ const Ticket = (props: TicketPropsType) => {
     setIsHover((isHoverVal) => !isHoverVal);
   };
 
-  // Ticket is owned
+  // Ticket is not owned by the current account
+  if (props.ticket.owner == account) {
+    return (
+      <div className='h-[210px] md:h-[400px]'>
+        <div
+          className={`mx-1 md:mx-2 ${
+            isHover || props.ticket.isSelected ? 'mt-10' : ''
+          }  flex h-[165px] min-w-[100px] cursor-pointer flex-col rounded border bg-moonbeam-pink px-4 py-2 transition-all md:h-[350px] md:min-w-[200px]`}
+        >
+          <div className='neonTextBlue rounded-full text-2xl md:text-4xl'>{props.ticket.id}</div>
+          <div className='flex grow items-center justify-center'>
+            <div className='mt-16 hidden md:block'>
+              <Image src={moonbeam} layout='fixed' height='100px' width='100px' alt='' />
+            </div>
+            <div className='mt-6 block md:hidden'>
+              <Image src={moonbeam} layout='fixed' height='50px' width='50px' alt='' />
+            </div>
+          </div>
+          <p className='mb-4 flex grow items-center justify-center text-center text-[10px] uppercase text-white md:text-xl'>
+            you own this ticket
+          </p>
+        </div>
+      </div>
+    );
+  }
+
+  // Ticket is owned by someone other than the current account
   if (isOwner && props.ticket.owner) {
     const address = props.ticket.owner;
     return (
